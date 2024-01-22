b1041d6528d06af6ba2c96e79095f08eef915f9a
Jonas9172 <105164575+Jonas9172@users.noreply.github.com>
2024-01-23 02:02:06 +0800

    bfs.py

---
+++ bfs.py
@@ -0,0 +1,24 @@
+direction = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 搜索的方向
+matrix = [[],[]]
+i, j = 0, 0  # 搜索的起始位置
+
+def bfs(i, j):
+
+    if i, j 满足某种条件:    # 有明确终点时使用，可有可无
+        return
+
+    queue = [[i, j]]  #本轮搜索的所有父端点
+
+    while len(queue) > 0:  #对于每一个父端点
+        x, y = queue.pop(0)
+
+        for direction_X, direction_Y in direction:   #对于一个父端点下的所有子端点
+            new_X = x + direction_X
+            new_Y = y + direction_Y
+
+            # 可通过改变和筛选matrix[new_X, new_Y]的值来避免重复或逆向搜索
+            if new_X, new_Y, matrix[new_X, new_Y] 满足某种条件:
+                #进行操作
+                queue.append([new_X, new_Y])
+
+bfs(i, j)

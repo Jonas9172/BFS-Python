b1135f56c78d0529956d48891a2fdf29631f27c0
Jonas9172 <105164575+Jonas9172@users.noreply.github.com>
2024-01-24 01:12:34 +0800

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -7,9 +7,9 @@ def bfs(i, j):
     if i, j 满足某种条件:    # 有明确终点时使用，可有可无
         return
 
-    queue = [[i, j]]  #本轮搜索的所有父端点
+    queue = [[i, j]]  #本轮搜索的所有父端点的序列，一开始是(i,j)
 
-    while len(queue) > 0:  #对于每一个父端点
+    while len(queue) > 0:  #探索所有父端点序列
         x, y = queue.pop(0)
 
         for direction_X, direction_Y in direction:   #对于一个父端点下的所有子端点
@@ -19,6 +19,6 @@ def bfs(i, j):
             # 可通过改变和筛选matrix[new_X, new_Y]的值来避免重复或逆向搜索
             if new_X, new_Y, matrix[new_X, new_Y] 满足某种条件:
                 #进行操作
-                queue.append([new_X, new_Y])
+                queue.append([new_X, new_Y])  # 将合适的子端点放入搜索序列
 
 bfs(i, j)
